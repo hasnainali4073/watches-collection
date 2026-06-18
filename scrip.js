bc3b6fb0de751063c1457b948ecc0a805fa697f2
Hasnain <Hasnainali4073@gmail.com>
2026-06-18 12:44:24 +0500

    ok

--- scrip.js
+++ scrip.js
@@ -1,4 +1,21 @@
-function max(){
-    
-    alert("Your form has been submitted successfully!");
-}
+ 
+ 
+ 
+ function handlePlusClick() {
+        const inputField = document.getElementById('my-custom-qty');
+        let currentNumber = parseInt(inputField.value) || 1;
+        
+        if (currentNumber < 50) { // Max limit 50 hai
+            inputField.value = currentNumber + 1;
+        }
+    }
+
+    // Minus Button ka function (Value kam karne ke liye)
+    function handleMinusClick() {
+        const inputField = document.getElementById('my-custom-qty');
+        let currentNumber = parseInt(inputField.value) || 1;
+        
+        if (currentNumber > 1) { // Min limit 1 hai (zero nahi hoga)
+            inputField.value = currentNumber - 1;
+        }
+    }
